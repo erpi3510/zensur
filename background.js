911c2e074144dcc79c3bbbcf894e7531276f3180
eric <ericedmond849@yahoo.fr>
2024-02-21 20:11:22 +0100

    hm

--- background.js
+++ background.js
@@ -1,76 +1,68 @@
 
-// Funktion, um die URL des Tabs zu überprüfen
 function checkTabURL(tabId, url) {
   console.log("Tab ID:", tabId, "URL:", url);
-  // Hier den Code einfügen, um die URL zu überprüfen und entsprechend zu handeln
+
   if (url) {
-    chrome.tabs.query({
-      active: true,
-      currentWindow: true
-    }, function (tabs, message, sender, ) {
-      var activeTab = tabs[0];
-      var activeTabUrl = activeTab.url;
+      chrome.tabs.query({
+          active: true,
+          currentWindow: true
+      }, function (tabs) {
+          var activeTab = tabs[0];
+          var activeTabUrl = activeTab.url;
 
-      fetch(chrome.runtime.getURL('blocked_urls.json'))
-        .then(response => response.json())
-        .then(data => {
-          const blockedUrls = data.urls;
-          for(let i = 0; i<= blockedUrls.length;i++){
-            if (activeTabUrl.includes(blockedUrls[i])) {
+          fetch('http://localhost:3003/urls')
+              .then(response => response.json())
+              .then(data => {
+                  // Durch die empfangenen Daten iterieren und nach übereinstimmenden URLs suchen
+                  let foundMatch = false; // Variable zum Verfolgen, ob eine Übereinstimmung gefunden wurde
+                  for (const item of data) {
+                      if (!foundMatch && activeTabUrl.includes(item.url)) { // Überprüfen, ob eine Übereinstimmung gefunden wurde und noch keine Übereinstimmung gefunden wurde
+                          const iconUrl = 'images/icon_16.png';
 
-              const iconUrl = 'images/icon_16.png';
-              //chrome.action.setPopup({popup: 'popup.html'});
-              //chrome.action.setIcon({path: {16: iconUrl, 48: iconUrl, 128: iconUrl}});
+                          chrome.notifications.create({
+                              type: 'basic',
+                              iconUrl: iconUrl,
+                              title: 'Achtung, diese Seite ist möglicherweise unter Zensur bedroht',
+                              message: 'Es gab eine Warnmeldung',
+                          }, function (notificationId) {
+                              chrome.notifications.onClicked.addListener(function (clickedNotificationId) {
+                                  if (clickedNotificationId === notificationId) {
+                                      chrome.tabs.create({ url: activeTabUrl });
+                                  }
+                              });
+                          });
 
-              console.log("Aktuelle URL des Tabs:6", activeTabUrl);
-              
+                          chrome.action.setIcon({
+                              path: {
+                                  "16": "images/icon_48.png",
+                                  "32": "images/icon_32.png",
+                                  "48": "images/icon_48.png",
+                                  "128": "images/icon_128.png"
+                              }
+                          });
 
-chrome.action.setIcon({
-  path: {
-    "16": "images/icon_48.png",
-    "48": "images/icon_48.png",
-    "128": "images/icon_128.png"
-  }
-});
-
-chrome.notifications.create({
-  type: 'basic',
-  iconUrl: iconUrl, // Hier den Pfad zu Ihrem Symbol angeben
-  title: 'Achtung diese Seite ist möglicherweise unter zensur bedroht',
-  message: 'ES gab folgende warn meldung',
-},function(notificationId){
-    chrome.notifications.onClicked.addListener(function(clickedNotificationId) {
-    if (clickedNotificationId === notificationId) {
-      // Hier die Aktion ausführen, wenn auf die Benachrichtigung geklickt wird
-      chrome.tabs.create({ url: 'https://www.tagesschau.de/'});
-
-    }
-  });
-});
-
-break;
-
-            }else{
-              chrome.action.setIcon({
-                path: {
-                  "16": "images/icon-16.png",
-                  "32": "images/icon-32.png",
-                  "48": "images/icon-48.png",
-                  "128": "images/icon-128.png"
-                }
-              });
-console.log(blockedUrls[0]);
-            }
-            
-          }
-          
-        })
-        .catch(error => console.error('Error fetching blocked URLs:', error));
-
-
-    });
+                          foundMatch = true; // Setzen Sie den Zustand auf "true", um anzuzeigen, dass eine Übereinstimmung gefunden wurde
+                          break; // Schleife verlassen, da eine Übereinstimmung gefunden wurde
+                      }else {
+                        chrome.action.setIcon({
+                            path: {
+                                "16": "images/icon-16.png",
+                                "32": "images/icon-32.png",
+                                "48": "images/icon-48.png",
+                                "128": "images/icon-128.png"
+                            }
+                        });
+                    }
+                  }
+              })
+              .catch(error => console.error('Error fetching blocked URLs:', error));
+      });
   }
 }
+
+
+
+
 
 // Benachrichtigung bei Tab-Erstellung
 chrome.tabs.onCreated.addListener(function (tab) {
